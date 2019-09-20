af162a21d12a0b74161c32a428e17c1842792582
lidaSusanSabu <lida.sabu@experionglobal.com>
2019-09-20 10:05:41 +0530

    made changes to stucture of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductContainerComponent } from './product-container/product-container.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductContainerComponent,
+    ProductComponent,
+    SearchComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
